7279046ab71b385e52854663aaf11315b62c2e46
raddevus <1radeutsch1@gmail.com>
2020-07-23 15:32:19 -0400

    fixed redirectURL - added sign out button.

--- app/src/main/java/app/actionmobile/usertracker/MainActivity.kt
+++ app/src/main/java/app/actionmobile/usertracker/MainActivity.kt
@@ -11,11 +11,17 @@ import androidx.core.content.ContextCompat
 import com.onelogin.oidc.Callback
 import com.onelogin.oidc.OIDCConfiguration
 import com.onelogin.oidc.OneLoginOIDC
+import com.onelogin.oidc.data.stores.OneLoginEncryptionManager
 import com.onelogin.oidc.login.SignInError
 import com.onelogin.oidc.login.SignInSuccess
+import com.onelogin.oidc.revoke.RevokeError
+import com.onelogin.oidc.revoke.RevokeSuccess
+import com.onelogin.oidc.userInfo.UserInfo
+import com.onelogin.oidc.userInfo.UserInfoError
 
 class MainActivity : AppCompatActivity() {
     var signInButton : Button? = null
+    var signOutButton : Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,29 +30,63 @@ class MainActivity : AppCompatActivity() {
         val config = OIDCConfiguration.Builder()
             .clientId("dab1c550-af23-0138-a003-0615bd700714174077")
             .issuer("https://actionmobile-dev.onelogin.com/oidc/2")
-            .redirectUrl("https://actionmobile.app://callback")
+            .redirectUrl("app.actionmobile.usertracker://callback")
             .scopes(listOf("openid"))
             .isDebug(true)
             .build()
 
         signInButton = findViewById(R.id.signInButton) as Button
+        signOutButton = findViewById(R.id.signOutbutton) as Button
 
         signInButton!!.setOnClickListener {
             Log.d("MainActivity", "sign-in button clicked.")
             OneLoginOIDC.initialize(this.applicationContext,config)
             val oidcClient = OneLoginOIDC.getClient()
+
             Log.d("MainActivity", "got client...")
             oidcClient.signIn(this,object : Callback<SignInSuccess, SignInError> {
                 override fun onSuccess(success: SignInSuccess) {
                     // The user has been authenticated successfully,
                     // the `success` param will contain the `SessionInfo` with the tokens ready to be used
                     Log.d("MainActivity", "token: $success.sessionInfo.idToken");
+                    //Log.d("MainActivity", success.sessionInfo)
                 }
 
                 override fun onError(loginError: SignInError) {
-                    Log.d("MainActivity", "message: $loginError.message");
+                    Log.d("MainActivity", "message: $loginError.message : $loginError.cause");
+
                 }
             });
+        }
+
+        signOutButton!!.setOnClickListener {
+            Log.d("MainActivity", "sign-out button clicked")
+            OneLoginOIDC.initialize(this.applicationContext,config)
+            Log.d("MainActivity", "initialized")
+
+            val oidcClient = OneLoginOIDC.getClient()
+            oidcClient.cancel()
+            Log.d("MainActivity", "got client")
+            oidcClient.getUserInfo(object : Callback<UserInfo,UserInfoError>{
+                override fun onSuccess(success: UserInfo) {
+                    Log.d("MainActivity","$success.name")
+                }
+                override fun onError(error: UserInfoError){
+                    Log.d("MainActivity", "info error")
+                }
+            })
+            Log.d("MainActivity", "got client for revoke")
+
+            oidcClient.revokeToken(object : Callback<RevokeSuccess, RevokeError> {
+                override fun onSuccess(success: RevokeSuccess) {
+                    Log.d("MainActivity", "token: $success.sessionInfo.idToken");
+                }
+
+                override fun onError(error: RevokeError) {
+                    Log.d("MainActivity", "message: $error.message");
+                }
+            });
+            oidcClient.cancel()
         }
 
     }
