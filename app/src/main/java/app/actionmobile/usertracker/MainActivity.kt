e9595c9b2cbcb7f0ceac53ac07abe74885be2e16
raddevus <1radeutsch1@gmail.com>
2020-07-23 23:51:11 -0400

    cleaned up code in this branch

--- app/src/main/java/app/actionmobile/usertracker/MainActivity.kt
+++ app/src/main/java/app/actionmobile/usertracker/MainActivity.kt
@@ -9,6 +9,7 @@ import android.widget.Button
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.onelogin.oidc.Callback
+import com.onelogin.oidc.OIDCClient
 import com.onelogin.oidc.OIDCConfiguration
 import com.onelogin.oidc.OneLoginOIDC
 import com.onelogin.oidc.login.SignInError
@@ -16,27 +17,28 @@ import com.onelogin.oidc.login.SignInSuccess
 
 class MainActivity : AppCompatActivity() {
     var signInButton : Button? = null
+    var oidcClient : OIDCClient? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val config = OIDCConfiguration.Builder()
-            .clientId("32044570-ad95-0138-e94b-0ac5d5c515b4173933")
+            .clientId("dab1c550-af23-0138-a003-0615bd700714174077")
             .issuer("https://actionmobile-dev.onelogin.com/oidc/2")
-            .redirectUrl("https://app.actionmobile.usertracker://callback")
+            .redirectUrl("app.actionmobile.usertracker://callback")
             .scopes(listOf("openid"))
             .isDebug(true)
             .build()
 
         signInButton = findViewById(R.id.signInButton) as Button
 
-        signInButton!!.setOnClickListener {
+        signInButton?.setOnClickListener {
             Log.d("MainActivity", "sign-in button clicked.")
             OneLoginOIDC.initialize(this.applicationContext,config)
-            val oidcClient = OneLoginOIDC.getClient()
+            oidcClient = OneLoginOIDC.getClient()
             Log.d("MainActivity", "got client...")
-            oidcClient.signIn(this,object : Callback<SignInSuccess, SignInError> {
+            oidcClient?.signIn(this,object : Callback<SignInSuccess, SignInError> {
                 override fun onSuccess(success: SignInSuccess) {
                     // The user has been authenticated successfully,
                     // the `success` param will contain the `SessionInfo` with the tokens ready to be used
